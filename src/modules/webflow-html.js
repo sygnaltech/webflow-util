7b4e9a976cec18cab19d1455c4dab9e158114038
Michael Wells <info@sygnal.com>
2021-09-19 07:09:45 +1200

    Added notes

--- src/modules/webflow-html.js
+++ src/modules/webflow-html.js
@@ -3,7 +3,12 @@
 // Sygnal Technology Group
 // http://sygnal.com
 
+// Simplest-case encoding for HTML5
 export var encodeHtml = function (text) {
+
+    // Important- this approach handles common scenarios,
+    // but does not handle quotes or special accented characters.
+    // See https://www.php.net/htmlspecialchars
 
     return text
         .replace(/&/g, "&amp;")
