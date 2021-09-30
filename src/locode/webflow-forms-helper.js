075f970e462458a33ea174f3badaf48bdd56d22c
Michael Wells <info@sygnal.com>
2021-09-30 17:30:55 +1300

    Initial locode form databinding helper

---
+++ src/locode/webflow-forms-helper.js
@@ -0,0 +1,19 @@
+
+/*
+ * webflow-forms-helper
+ * 
+ * Sygnal Technology Group
+ * http://sygnal.com
+ * 
+ * LO-CODE Helper class to simplify form functions.
+ */
+
+import { dataBindAllFormSelects } from '../modules/webflow-form.js';
+
+export var dataBindAllForms = function () {
+
+    dataBindAllFormSelects();
+
+}
+
+
