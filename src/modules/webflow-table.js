bafe339f1b2361451acb9a7fe728854f0c79b7b7
Michael Wells <info@sygnal.com>
2021-10-01 01:12:47 +1300

    Full options support implemented, themes, html encoding, striping and backgroundColor

--- src/modules/webflow-table.js
+++ src/modules/webflow-table.js
@@ -1,26 +1,20 @@
 
-// v2.1
-// Sygnal Technology Group
-// http://sygnal.com
+/*
+ * Sygnal Technology Group
+ * http://sygnal.com
+ */
 
 import { getCsv, csvToJson } from './webflow-data.js';
 import { encodeHtml } from './webflow-html.js';
 
-    //function isNotEmpty(row) {
-    //    return row !== "";
-    //}
-
-// https://www.w3.org/wiki/JavaScript_best_practices#Allow_for_configuration_and_translation
-//let renderOptions = {
-//    allowHtml: false,
-//    headers: null
-//}
-
 var tableRenderOptions = {
     encodeHtml: true,
-    theme: 'table1',
+    theme: 'default',
     headers: null,
-    responsive: true
+    responsive: true,
+    striped: false,
+    bordered: true,
+    backgroundColor: null // transparent
 }
 
 export var renderTableFromCsvUrl = function (elem, url, options = {}) {
@@ -32,7 +26,11 @@ export var renderTableFromCsvUrl = function (elem, url, options = {}) {
     var csv = getCsv(url);
     //.then(function (response) {
 
-        renderTableFromCsv(elem, csv, options);
+    renderTableFromCsv(
+        elem,
+        csv,
+        options
+    );
 
     //}, function (error) {
     //    console.error(error);
@@ -48,7 +46,11 @@ export var renderTableFromCsv = function (elem, csv, options = {}) {
 
     var json = csvToJson(csv);
 
-    renderTableFromJson(elem, json, options);
+    renderTableFromJson(
+        elem,
+        json,
+        options
+    );
 
 }
 
@@ -64,7 +66,23 @@ export var renderTableFromJson = function (elem, json, options = {}) {
 // Assumes array is uniform, with the same fields in each item.
 export var renderTableFromArray = function (elem, arr, options = {}) {
 
+    // Consolidate options + defaults into settings
     var settings = $.extend({}, tableRenderOptions, options);
+
+
+    // dynamically add theme CSS
+    // e.g. <link href="https://localhost/dist/theme/table1/table.css" rel="stylesheet" type="text/css" />
+    // BUG: prevent multiple loads
+    if (settings.theme) {
+        var stylesheet = $("<link>", {
+            rel: "stylesheet",
+            type: "text/css",
+            href: `https://localhost/dist/theme/${settings.theme}/table.css`
+        });
+    stylesheet.appendTo("head");
+    }
+
+
 
     // If no header specified, 
     // infer Header Row automatically from first array item
@@ -84,10 +102,24 @@ export var renderTableFromArray = function (elem, arr, options = {}) {
     console.log($`Headers = {val}`, headers);
 
     var html = [];
-    html.push('<div>');
 
-    settings
+    // Putting all clases on DIV for simplicity
+    var divClass = [];
+    if (settings.responsive)
+        divClass.push('wfu-table-responsive'); // div
+    divClass.push('wfu-table'); // table
+    if (settings.striped)
+        divClass.push('wfu-striped'); // table
+    if (settings.bordered)
+        divClass.push('wfu-bordered'); // table
 
+    var divStyle = [];
+    if (settings.backgroundColor)
+        divStyle.push(`background-color: ${settings.backgroundColor}`);
+
+    html.push("<!-- Sygnal Webflow Utils -->");
+    html.push(`<div class='${divClass.join(" ")}' style='${divStyle.join("; ")}'>`);
+    
     //if (options.responsive ?? tableRenderOptions.responsive)
     //if (options.theme ?? tableRenderOptions.theme)
     //if (options.encodeHtml ?? tableRenderOptions.encodeHtml)
@@ -99,20 +131,26 @@ export var renderTableFromArray = function (elem, arr, options = {}) {
     html.push('<tr>');
     for (var key in headers) {
         html.push('<th>');
-        html.push(encodeHtml(headers[key])); // HTML-encoded
+        if (settings.encodeHtml)
+            html.push(encodeHtml(headers[key])); // HTML-encoded
+        else
+            html.push(headers[key]); 
         html.push('</th>');
     }
     html.push('</tr>');
     html.push('</thead>');
 
-
+    // Body
     html.push('<tbody>');
     for (var row = 0; row < arr.length; row++) {
 
         html.push('<tr>');
         for (var key in headers) {
             html.push('<td>');
-            html.push(encodeHtml(arr[row][headers[key]])); // HTML-encoded
+            if (settings.encodeHtml)
+                html.push(encodeHtml(arr[row][headers[key]])); // HTML-encoded
+            else
+                html.push(arr[row][headers[key]]); 
             html.push('</td>');
         }
         html.push('</tr>');
