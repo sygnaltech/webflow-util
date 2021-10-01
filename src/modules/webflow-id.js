24339c819a72c1f6da9ea912568230e7cb5a9f8a
Michael Wells <info@sygnal.com>
2021-10-02 10:04:21 +1300

    Added UUID v4 support

--- src/modules/webflow-id.js
+++ src/modules/webflow-id.js
@@ -34,5 +34,20 @@ export var generateHtmlId = function () {
     return id;
 }
 
+// Generates a UUID v4
+// Using the cryptographic API 
+// https://stackoverflow.com/a/2117523/1849956
+export var generateUuid = function () {
 
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+//function uuidv4() {
+//    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+//        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+//        return v.toString(16);
+//    });
+//}
 
