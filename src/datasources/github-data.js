9ce0eec73ee6f1bad5fb5af80cc99f714286dbc6
Michael Wells <info@sygnal.com>
2021-10-22 19:33:25 +1300

    Github datasource completed, with promises

--- src/datasources/github-data.js
+++ src/datasources/github-data.js
@@ -22,8 +22,6 @@ export var getGithubRepoTags = function (userOrg, repo) {
         const url = getGithubRepoTagsUrl(userOrg, repo);
 
         $.get(url).done(data => {
-            //        const versions = data.sort((v1, v2) => semver.compare(v2.name, v1.name));
-            //        $('#result').html(versions[0].name);
             resolve(data);
         });
 
@@ -31,18 +29,20 @@ export var getGithubRepoTags = function (userOrg, repo) {
 
 }
 
-//import { dataBindAllFormSelects } from '../modules/webflow-form.js';
+export var getGithubRepoTagLatest = function (userOrg, repo) {
 
-//export var dataBindAllForms = function () {
+    return new Promise((resolve, reject) => {
 
-//    dataBindAllFormSelects();
+        getGithubRepoTags(userOrg, repo)
+            .then((res) => {
+                resolve(res[0].name);
+            }, (err) => {
 
-//}
+            });
 
+    });
 
-
-// const gitHubPath = 'dandv/local-iso-dt';  // example repo
-// const url = 'https://api.github.com/repos/' + gitHubPath + '/tags';
+}
 
 //$.get(url).done(data => {
 //    const versions = data.sort((v1, v2) => semver.compare(v2.name, v1.name));
