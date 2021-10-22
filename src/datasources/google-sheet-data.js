06665d5301ce53a1db86a19232160e24d26b43d4
Michael Wells <info@sygnal.com>
2021-10-22 20:20:00 +1300

    Refactored Google Sheet datasource

---
+++ src/datasources/google-sheet-data.js
@@ -0,0 +1,38 @@
+
+/*
+ * google-sheet-data
+ *
+ * Sygnal Technology Group
+ * http://sygnal.com
+ *
+ * Extracts information from Google Sheets, and returns as JSON
+ */
+
+import { csvToJson } from '/src/modules/webflow-data.js';
+import { csvToObjects } from '/src/modules/webflow-data-csv.js';
+import { getCsvAsJson } from '/src/modules/webflow-data.js';
+
+// Returns a Url to retrieve the CSV version of a Google Sheet
+// The sheet must be publicly accessible.
+export var getGoogleSheetCsvUrl = function (id) {
+
+    return `https://docs.google.com/spreadsheets/d/${id}/export?format=csv`;
+}
+
+// Returns all data as JSON from Sheet1
+export var getGoogleSheetData = function (id) {
+
+    return new Promise((resolve, reject) => {
+
+        var url = getGoogleSheetCsvUrl(id);
+        console.log(url);
+
+        var json = getCsvAsJson(
+            getGoogleSheetCsvUrl(id),
+            true
+        );
+
+        resolve(json);
+
+    });
+}
