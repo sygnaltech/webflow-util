d09326585172627800ffc2c0f8df5e9345b57c29
Michael Wells <info@sygnal.com>
2021-11-23 17:12:20 +1300

    Format JSON

--- src/modules/webflow-html.js
+++ src/modules/webflow-html.js
@@ -98,3 +98,49 @@ export var applyDynamicAttributes = function () {
 
 }
 
+export var formatJson = function (json) {
+
+    // Convert JSON to string
+    if (typeof json != 'string') {
+        json = JSON.stringify(json, undefined, 2);
+    }
+
+    return json;
+}
+
+export var formatJsonAsHtml = function (json) {
+
+    // Convert JSON to string
+    json = formatJson(json);
+
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+    // Add JSON styling classes
+    return json.replace(
+        /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+        function (match) {
+            var cls = 'wfu-json-number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'wfu-json-key';
+                } else {
+                    cls = 'wfu-json-string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'wfu-json-boolean';
+            } else if (/null/.test(match)) {
+                cls = 'wfu-json-null';
+            }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+
+}
+
+export var displayJsonAsHtml = function (el, json) {
+
+    $(el).html("<pre class='wfu-code'></pre>");
+    $(el).children("pre").html(
+        formatJsonAsHtml(json)
+    );
+
+}
