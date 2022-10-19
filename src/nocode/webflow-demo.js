3688b29fd3ff898619d1310345a613ac8d16cba6
Michael Wells <info@sygnal.com>
2022-10-19 15:51:50 +1300

    Logging removed

--- src/nocode/webflow-demo.js
+++ src/nocode/webflow-demo.js
@@ -12,10 +12,8 @@ import { updateHrefToWebflowPreviewLink } from '../modules/webflow-demo.js';
 
 $(function () {
 
-    console.log("running.");
-
     $("a[wfu-demo-link]").each(function () {
-        console.log("parsing.");
+
         updateHrefToWebflowPreviewLink($(this));
 
     });
