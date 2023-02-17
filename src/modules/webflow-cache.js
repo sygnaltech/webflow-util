38e3941cbca05203d54536b0d45d6ffd61fcf3c1
Michael Wells <info@sygnal.com>
2023-02-17 15:22:24 +1300

    Bugfix: returns initial calc async reliably

--- src/modules/webflow-cache.js
+++ src/modules/webflow-cache.js
@@ -19,21 +19,15 @@ var defaultWfuCacheItemConfig = {
     name: undefined, 
     updateFnAsync: undefined,   
   
-    debug: false, // Debugging mode
-
 } 
 
 export class WfuCacheItem {
-
-    console = new WfuDebug();
 
     config; // Optional config
 
     constructor(config = {}) {
 
         this.config = $.extend({}, defaultWfuCacheItemConfig, config);
-
-        this.console.enabled = this.config.debug; 
 
     }
 
@@ -61,32 +55,36 @@ export class WfuCache {
 
     }
 
-    async getAsync(value) {
+    async getAsync(valueName) {
 
-//      console.log("getAsync", value);
-      
-      var valueHandler = this.config.val[value];
-//      console.log("valueHandler", valueHandler);
-      
-      var v  = sessionStorage.getItem(value);
-      // console.log("sessionStorage.getItem", v); 
-      
-      // No current value
-      if (v == null || v == undefined) { 
-
-        // console.log("updateFnAsync");
+        this.console.group(`getAsync - "${valueName}"`);
         
-        valueHandler.config.updateFnAsync().then(r => {
-          sessionStorage.setItem(value, r);
-          // console.log("sessionStorage.setItem", value, r); 
-          return r;
-        }); 
-//        console.debug("no such var");   
-      } else {
-        // console.log("value found", v); 
-      }
+        var valueHandler = this.config.val[valueName];
+        this.console.debug("valueHandler", valueHandler);
+        
+        var returnValue = sessionStorage.getItem(valueName);
+        this.console.debug("cached? sessionStorage.getItem", returnValue); 
+      
+        const that = this;
 
-      return v; 
+        // Not cached
+        // go get this value 
+        if (returnValue == null || returnValue == undefined) { 
+            
+            // Call valueHandler function to calculate 
+            returnValue = await valueHandler.config.updateFnAsync().then(r => {
+                sessionStorage.setItem(valueName, r);
+                that.console.debug("sessionStorage.setItem", valueName, r); 
+                that.console.debug("calculated", r); 
+                return r;
+                }); 
+
+        }
+
+        this.console.debug("returning", returnValue); 
+
+        this.console.groupEnd();
+        return returnValue; 
     }
 
 }
