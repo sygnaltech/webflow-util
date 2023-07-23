caaf72765b8dd856361fdbaac39891d7e53fff68
Michael Wells <info@sygnal.com>
2023-07-23 03:50:11 +0000

    Build

---
+++ dist/data/google-docs.js
@@ -0,0 +1,106 @@
+(() => {
+  // src/webflow-core/debug.ts
+  var Sa5Debug = class {
+    constructor(label) {
+      this.localStorageDebugFlag = "sa5-debug";
+      this._enabled = false;
+      this._label = label;
+    }
+    get persistentDebug() {
+      return Boolean(localStorage.getItem(this.localStorageDebugFlag));
+    }
+    set persistentDebug(active) {
+      if (active) {
+        localStorage.setItem(this.localStorageDebugFlag, "true");
+        console.debug("sa5-core debug enabled (persistent).");
+      } else {
+        localStorage.removeItem(this.localStorageDebugFlag);
+        console.debug("sa5-core debug disabled (persistent).");
+      }
+    }
+    get enabled() {
+      var wfuDebugValue = Boolean(localStorage.getItem(this.localStorageDebugFlag));
+      wfuDebugValue = wfuDebugValue || this._enabled;
+      return wfuDebugValue;
+    }
+    set enabled(active) {
+      this._enabled = active;
+    }
+    group(name) {
+      if (this.enabled)
+        console.group(name);
+    }
+    groupEnd() {
+      if (this.enabled)
+        console.groupEnd();
+    }
+    debug(...args) {
+      if (this.enabled)
+        console.debug(this._label, ...args);
+    }
+  };
+
+  // src/webflow-core.ts
+  var Sa5Core = class {
+    constructor() {
+      this.handlers = [];
+    }
+    init() {
+      this.initDebugMode();
+    }
+    initDebugMode() {
+      const debugParamKey = "sa-debug";
+      let params = new URLSearchParams(window.location.search);
+      let hasDebug = params.has(debugParamKey);
+      if (hasDebug) {
+        let wfuDebug = new Sa5Debug(`sa5 init`);
+        wfuDebug.persistentDebug = this.stringToBoolean(params.get(debugParamKey));
+      }
+    }
+    stringToBoolean(str) {
+      const truthyValues = ["1", "true", "yes"];
+      const falsyValues = ["0", "false", "no"];
+      if (truthyValues.indexOf(str.toLowerCase()) !== -1) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    static startup(module = null) {
+      console.debug("sa5core", "startup");
+      if (!(window["sa5"] instanceof Sa5Core)) {
+        console.debug("CORE");
+        var core = new Sa5Core();
+        if (Array.isArray(window["sa5"]))
+          core.handlers = window["sa5"];
+        window["sa5"] = core;
+      }
+      if (module) {
+        console.debug("Registered module", module.name);
+        window["sa5"][module.name] = module;
+      }
+    }
+    push(o) {
+      this.handlers.push(o);
+    }
+  };
+  Sa5Core.startup();
+
+  // src/data/google-docs.ts
+  var Sa5GoogleDocs = class {
+    constructor() {
+    }
+    static getId(url) {
+      var prefix = "https://docs.google.com/document/d/";
+      if (url.startsWith(prefix)) {
+        console.log(url);
+        url = url.substring(prefix.length);
+        console.log(url);
+        return url.split("/")[0];
+      }
+      console.error(`Unknown google docs URL format - ${url}`);
+    }
+  };
+  Sa5Core.startup(Sa5GoogleDocs);
+})();
+//# sourceMappingURL=google-docs.js.map
---
+++ dist/webflow-storage.js
@@ -0,0 +1,137 @@
+(() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __propIsEnum = Object.prototype.propertyIsEnumerable;
+  var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+  var __spreadValues = (a, b) => {
+    for (var prop in b ||= {})
+      if (__hasOwnProp.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    if (__getOwnPropSymbols)
+      for (var prop of __getOwnPropSymbols(b)) {
+        if (__propIsEnum.call(b, prop))
+          __defNormalProp(a, prop, b[prop]);
+      }
+    return a;
+  };
+
+  // src/webflow-core/debug.ts
+  var Sa5Debug = class {
+    constructor(label) {
+      this.localStorageDebugFlag = "sa5-debug";
+      this._enabled = false;
+      this._label = label;
+    }
+    get persistentDebug() {
+      return Boolean(localStorage.getItem(this.localStorageDebugFlag));
+    }
+    set persistentDebug(active) {
+      if (active) {
+        localStorage.setItem(this.localStorageDebugFlag, "true");
+        console.debug("sa5-core debug enabled (persistent).");
+      } else {
+        localStorage.removeItem(this.localStorageDebugFlag);
+        console.debug("sa5-core debug disabled (persistent).");
+      }
+    }
+    get enabled() {
+      var wfuDebugValue = Boolean(localStorage.getItem(this.localStorageDebugFlag));
+      wfuDebugValue = wfuDebugValue || this._enabled;
+      return wfuDebugValue;
+    }
+    set enabled(active) {
+      this._enabled = active;
+    }
+    group(name) {
+      if (this.enabled)
+        console.group(name);
+    }
+    groupEnd() {
+      if (this.enabled)
+        console.groupEnd();
+    }
+    debug(...args) {
+      if (this.enabled)
+        console.debug(this._label, ...args);
+    }
+  };
+
+  // src/webflow-core.ts
+  var Sa5Core = class {
+    constructor() {
+      this.handlers = [];
+    }
+    init() {
+      this.initDebugMode();
+    }
+    initDebugMode() {
+      const debugParamKey = "sa-debug";
+      let params = new URLSearchParams(window.location.search);
+      let hasDebug = params.has(debugParamKey);
+      if (hasDebug) {
+        let wfuDebug = new Sa5Debug(`sa5 init`);
+        wfuDebug.persistentDebug = this.stringToBoolean(params.get(debugParamKey));
+      }
+    }
+    stringToBoolean(str) {
+      const truthyValues = ["1", "true", "yes"];
+      const falsyValues = ["0", "false", "no"];
+      if (truthyValues.indexOf(str.toLowerCase()) !== -1) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    static startup(module = null) {
+      console.debug("sa5core", "startup");
+      if (!(window["sa5"] instanceof Sa5Core)) {
+        console.debug("CORE");
+        var core = new Sa5Core();
+        if (Array.isArray(window["sa5"]))
+          core.handlers = window["sa5"];
+        window["sa5"] = core;
+      }
+      if (module) {
+        console.debug("Registered module", module.name);
+        window["sa5"][module.name] = module;
+      }
+    }
+    push(o) {
+      this.handlers.push(o);
+    }
+  };
+  Sa5Core.startup();
+
+  // src/webflow-storage.ts
+  var defaultConfig = {
+    storageMethod: 0 /* sessionStorage */
+  };
+  var Sa5Storage = class {
+    constructor(customConfig = {}) {
+      this.config = __spreadValues(__spreadValues({}, defaultConfig), customConfig);
+      this.debug = new Sa5Debug("sa5-storage");
+    }
+    setValue(key, value) {
+      sessionStorage.setItem(
+        key,
+        value
+      );
+    }
+    getValue(key) {
+      switch (this.config.storageMethod) {
+        case 2 /* cookies */:
+          break;
+        case 1 /* localStorage */:
+          break;
+        case 0 /* sessionStorage */:
+          break;
+      }
+      return sessionStorage.getItem(
+        key
+      );
+    }
+  };
+  Sa5Core.startup(Sa5Storage);
+})();
+//# sourceMappingURL=webflow-storage.js.map
