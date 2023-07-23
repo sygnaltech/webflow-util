2334fe4abcfde4223a7e655d1aef8cfd3a727818
Michael Wells <info@sygnal.com>
2023-07-23 19:56:57 +0000

    Docs

--- src/webflow-html/breakpoints.ts
+++ src/webflow-html/breakpoints.ts
@@ -94,3 +94,21 @@ export class Sa5Breakpoints {
 
 
   
+// "site": {
+//     "mediaQueries": [{
+//         "key": "main",
+//         "min": 992,
+//         "max": 10000
+//     }, {
+//         "key": "medium",
+//         "min": 768,
+//         "max": 991
+//     }, {
+//         "key": "small",
+//         "min": 480,
+//         "max": 767
+//     }, {
+//         "key": "tiny",
+//         "min": 0,
+//         "max": 479
+//     }]
