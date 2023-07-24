8c84a8275cde5b65e5466076e851b3a25b18811c
Michael Wells <info@sygnal.com>
2023-07-24 12:02:05 +0000

    Data binding

--- src/nocode/webflow-data.ts
+++ src/nocode/webflow-data.ts
@@ -8,30 +8,29 @@
  * NO-CODE version, keys off of [wfu] attributes.
  */
 
-import { WfuQuery, WfuRelativeLinkFixup, WfuTargetLinks } from '../webflow-url';
+//import { WfuQuery, WfuRelativeLinkFixup, WfuTargetLinks } from '../webflow-url';
 import { Sa5Core } from '../webflow-core'; 
 import { Sa5Debug } from '../webflow-core/debug';
+
+import { loadAllData } from '../modules/webflow-data.js';
+import { dataBindAllForms } from '../modules/webflow-form.js';
+
+// export var dataBindAll = function () {
 
 const init = () => { 
 
     new Sa5Core().init();
 
     // Initialize debugging
-    let debug = new Sa5Debug("sa5-url");
+    let debug = new Sa5Debug("sa5-data");
     debug.debug ("Initializing");
 
-    const wfuQuery = new WfuQuery();
-    wfuQuery.processAll();
+    // Create database
+    var db = loadAllData();
 
-    // Fixup relative links from the CMS
-    // HACK: 
-    const wfuRelativeLinkFixup = new WfuRelativeLinkFixup();
-    wfuRelativeLinkFixup.processAll();
-
-    // Target external links to _blank
-    const wfuTargetLinks = new WfuTargetLinks();
-    wfuTargetLinks.processAll();
+    // Bind all form elements
+    dataBindAllForms(db);
 
 }
-  
+
 document.addEventListener("DOMContentLoaded", init)
